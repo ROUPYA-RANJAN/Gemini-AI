104c557415daaa3bf6a6a5d4b142e65320a4122e
ROUPYA RANJAN PRUSTY <131539218+ROUPYA-RANJAN@users.noreply.github.com>
2024-10-28 10:20:50 +0530

    Update Main.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -27,7 +27,7 @@ const Main = () => {
 					<>
 						<div className="greet">
 							<p>
-								<span>Hello , Dev </span>
+								<span>Hello , Human</span>
 							</p>
 							<p>How Can i Help You Today?</p>
 						</div>
